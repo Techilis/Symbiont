1b583ea8a2ef47b9222f383b5955a795184b5787
gluonnus@hotmail.com <gluonnus@hotmail.com>
2019-09-20 12:42:55 +0800

    Chapt 6

---
+++ Symbiont/Symbiont.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Engine;
+
+namespace Symbiont
+{
+    public partial class Symbiont : Form
+    {
+        //Creates place to store Player object, underscore to signify private variable
+        private Player _player;
+        public Symbiont() //Constructor
+        {
+            InitializeComponent();
+
+            //Creates new player object and assigns obect to the _player variable created earlier
+            _player = new Player();
+            _player.CurrentHitPoints = 10;
+            _player.MaximumHitPoints = 10;
+            _player.Gold = 20;
+            _player.ExperiencePoints = 0;
+            _player.Level = 1;
+
+            lblHitPoints.Text = _player.CurrentHitPoints.ToString();
+            lblGold.Text = _player.Gold.ToString();
+            lblExperience.Text = _player.ExperiencePoints.ToString();
+            lblLevel.Text = _player.Level.ToString();
+        }
+
+        private void Label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
