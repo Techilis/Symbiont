a66bc7b2c8d052a9514d391e9abbf19bc3d62749
gluonnus@hotmail.com <gluonnus@hotmail.com>
2019-09-25 19:04:52 +0800

    Inheritance

--- Engine/HealingPoition.cs
+++ Engine/HealingPoition.cs
@@ -4,11 +4,8 @@ using System.Text;
 
 namespace Engine
 {
-    public class HealingPoition
+    public class HealingPoition : Item
     {
-        public int ID { get; set; }
-        public string Name { get; set; }
-        public string NamePlural { get; set; }
         public int AmountToHeal { get; set; }
     }
 }
